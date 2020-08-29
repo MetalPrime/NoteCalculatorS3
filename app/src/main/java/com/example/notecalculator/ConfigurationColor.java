51259fbce5516d1270f7f9a6d009364c3ba9023f
Jhon Garcia <metalprime57@gmail.com>
2020-08-28 22:15:33 -0500

    First Commit, Layout Especification

---
+++ app/src/main/java/com/example/notecalculator/ConfigurationColor.java
@@ -0,0 +1,14 @@
+package com.example.notecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigurationColor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration_color);
+    }
+}
